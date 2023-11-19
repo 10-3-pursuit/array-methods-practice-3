b5bc1cffbc00921fd349e58d14ead615382baf09
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-11-19 15:44:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+ * Creates an array containing only the names of the Pokémon.
+ * @returns {string[]} An array of Pokémon names.
+ */
+const pokemonNames = () => {};
+
+/**
+ * Finds all water type Pokémon.
+ * @returns {Object[]} An array of water type Pokémon.
+ */
+const waterTypePokemons = () => {};
+
+/**
+ * Finds a Pokémon with a specific ID.
+ * @param {number} id - The ID of the Pokémon to find.
+ * @returns {Object|null} The Pokémon object if found, otherwise null.
+ */
+const findPokemonById = () => {};
+
+/**
+ * Counts how many Pokémon have the move 'Tackle'.
+ * @returns {number} The count of Pokémon with 'Tackle' move.
+ */
+const countTackleMove = () => {};
+/**
+ * Creates an array of all unique moves.
+ * @returns {string[]} An array of unique Pokémon moves.
+ */
+const allMoves = () => {};
+/**
+ * Filters Pokémon that have the move 'Growl'.
+ * @returns {Object[]} An array of Pokémon with 'Growl' move.
+ */
+const pokemonWithGrowl = () => {};
+/**
+ * Finds the first Pokémon with the move 'Water Gun'.
+ * @returns {Object|null} The first Pokémon with 'Water Gun' move, or null if not found.
+ */
+const firstWaterGunPokemon = () => {};
+/**
+ * Creates an array of URLs of the Pokémon images.
+ * @returns {string[]} An array of image URLs.
+ */
+const pokemonImages = () => {};
+
+/**
+ * Creates an object grouping Pokémon by their type.
+ * @returns {Object} An object with keys as types and values as arrays of Pokémon.
+ */
+const groupByType = () => {};
+
+/**
+ * Calculates the average ID of all Pokémon.
+ * @returns {number} The average ID.
+ */
+const averageId = () => {};
+/**
+ * Finds the index of a Pokémon in the array based on its name.
+ * @param {string} name - The name of the Pokémon to find.
+ * @returns {number} The index of the Pokémon in the array, or -1 if not found.
+ */
+const findPokemonIndexByName = () => {};
